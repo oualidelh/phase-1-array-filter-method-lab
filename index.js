737dc8ac88ab09053d607c1ed3033372683f6732
oualidelhouari <elhouarioualid@gmail.com>
2023-08-08 22:48:29 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, name) {
+
+    const lowerCaseName = name.toLowerCase();
+    return array.filter(item => item.toLowerCase() === lowerCaseName);
+
+}
+function fuzzyMatch(array, name) {
+
+    const firstCharName = name.charAt(0);
+    return array.filter(item => item.charAt(0) === firstCharName);
+
+}
+function matchName(array, name) {
+
+    return array.filter(item => item.name === name);
+}
